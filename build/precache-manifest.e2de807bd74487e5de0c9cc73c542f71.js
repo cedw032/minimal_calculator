bae8e1245b4a1cc9b3e4a10ff2e836afd99a1df8
Chad Edwards <chad@fergusapp.com>
2019-05-05 21:48:54 +1200

    Build

---
+++ build/precache-manifest.e2de807bd74487e5de0c9cc73c542f71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c60b199a9ad1400c47d14b233995fea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f232e97704d71aa392c",
+    "url": "/static/css/main.55341c7a.chunk.css"
+  },
+  {
+    "revision": "5c7badd242f9ece752a2",
+    "url": "/static/js/2.adfc45b8.chunk.js"
+  },
+  {
+    "revision": "6f232e97704d71aa392c",
+    "url": "/static/js/main.53a1fbb9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
